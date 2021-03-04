9aac32c0572ba8566d0fda2a5ac6986555b67f5a
Tal Feiner <talfe@post.bgu.ac.il>
2021-03-04 21:13:21 +0200

    update

--- scripts/navigation_services_with_feedback.py
+++ scripts/navigation_services_with_feedback.py
@@ -24,9 +24,9 @@ def planning_cobra_center(pose="cobra_center_pose"):
 def set_toroso(hight=0.01):
     pub_torso_cmd = rospy.Publisher("/torso_effort_controller/command", Float64, queue_size=1)
     pub_torso_cmd.publish(Float64(hight))
-    for ii in range(3):
+    for __ in range(3):
         pub_torso_cmd.publish(Float64(hight))
-        rospy.sleep(0.1)
+        rospy.sleep(0.01)
     rospy.sleep(1)
 
 
